ef93ad71ddbb4c8845ec16b627b97c7de0902b21
Travis Bickle <kak.serpom.po.yaitsam@gmail.com>
2010-06-23 15:27:58 +0400

    first commit

---
+++ html/files/chat.js
@@ -0,0 +1,435 @@
+$.chat =
+{
+ "serverUrl": {
+                ws: 'ws://'+document.domain+'/Chat',
+                comet  : 'http://'+document.domain+'/WebSocketOverCOMET/?_route=Chat',
+                polling : 'http://'+document.domain+'/WebSocketOverCOMET/?_route=Chat'
+              },
+ "status": null,
+ "ws": null,
+ "onStatusReady": null,
+ "cmdHistory": [],
+ "cmdHistoryPointer": null,
+ "tags": [],
+ "userlist": {},
+ "tagsDefault": ["phpdaemon"],
+ "username": null,
+ "lastRecipients": null,
+ "userlistUpdateTimeout": null,
+ "sentBytes": 0,
+ "recvBytes": 0,
+ "sendPacket": function(packet)
+ {
+  var s = $.toJSON(packet);
+  $.chat.sentBytes += s.length;
+  try {$.chat.ws.send(s);}
+  catch (err) {}
+ },
+ "setForms": function()
+ {
+  if ($.chat.username != null)
+  {
+   $('.yourusername').text($.chat.username);
+   $('#inputForm').show();
+   $('#loginForm').hide();
+  }
+  else
+  {
+   $('#inputForm').hide();
+   $('#loginForm').show();
+  }
+  $('.yourtags').text($.chat.tags.toString());
+ },
+ "setRecipient": function(username)
+ {
+  $.chat.lastRecipients = '@'+username;
+  var e = $('#inputMessage').val().split(': ',2);
+  $('#inputMessage').val($.chat.lastRecipients+': '+(e[1] != null?e[1]:''));
+  $('#inputMessage').focus();
+ },
+ "updatedUserlist": function()
+ {
+  $('#userlist').html('');
+  var found = false;
+  for (var k in $.chat.userlist)
+  {
+   var u = $.chat.userlist[k];
+   var title = htmlspecialchars(k);
+   var statuses = '';
+   for (var j in u)
+   {
+    if (u[j].statusmsg != null) {statuses += htmlspecialchars(u[j].statusmsg)+' ... ';}
+   }
+   if (statuses != '') {title += ' - '+statuses;}
+   title += ' - Tags: '+u[j].tags;
+   $('#userlist').append('<p><a href="#" title="'+title+'" onclick="$.chat.setRecipient('+htmlspecialchars($.toJSON(k))+'); return false">'+htmlspecialchars(k)+'</a></p>');
+   found = true;
+  }
+  if (!found)
+  {
+   if ($.chat.tags.toString() != '') {$('#userlist').html("<i>No users tagged '"+htmlspecialchars($.chat.tags.toString())+"'.</i>");}
+   else {$('#userlist').html("<i>You\'re not listed on any tags.</i>");}
+  }
+  else
+  {
+   $('#userlist a').tooltip({ 
+     track: true, 
+     delay: 0, 
+     showURL: false, 
+     showBody: " - ", 
+     fade: 250 
+   });
+  }
+ },
+ "setTags": function(tags)
+ {
+  $.chat.tags = tags;
+  $.chat.sendPacket({
+   "cmd": "setTags",
+   "tags": tags
+  });
+ },
+ "keepalive": function()
+ {
+  $.chat.sendPacket({
+   "cmd": "keepalive"
+  });
+ },
+ "lastTS": 0,
+ "initConnect": function()
+ {
+  $.chat.connect();
+  setInterval(function()
+  {
+   $.chat.connect();
+   $('.sentDataCounter').html(fsize($.chat.sentBytes));
+   $('.recvDataCounter').html(fsize($.chat.recvBytes));
+  },1000);
+ },
+ "initInputForm": function()
+ {
+  $('#inputMessage')
+  .keyboard('esc',function (event) {$('#inputMessage').val('');})
+  .keyboard('aup, tab',{"preventDefault": true},function (event)
+  {
+   if ($.chat.cmdHistoryPointer <= 0)
+   {
+    if ($.chat.cmdHistoryPointer < 0) {$.chat.cmdHistoryPointer = 0;}
+    return;
+   }
+   --$.chat.cmdHistoryPointer;
+   $('#inputMessage').val($.chat.cmdHistory[$.chat.cmdHistoryPointer]);
+  })
+  .keyboard('adown',function (event)
+  {
+   if ($.chat.cmdHistory.length < $.chat.cmdHistoryPointer) {}
+   else if ($.chat.cmdHistory.length == $.chat.cmdHistoryPointer+1)
+   {
+    $('#inputMessage').val('');
+    ++$.chat.cmdHistoryPointer;
+    return;
+   }
+   else
+   {
+    ++$.chat.cmdHistoryPointer;
+    $('#inputMessage').val($.chat.cmdHistory[$.chat.cmdHistoryPointer]);
+   }
+  });
+  $('#inputForm').submit(function()
+  {
+   setTimeout(function()
+   {
+    if ($('#inputMessage').val() == '') {return;}
+    if ($('#inputMessage').val() == '/')
+    {
+     if ($.chat.cmdHistory.length > 0) {$('#inputMessage').val($.chat.cmdHistory[$.chat.cmdHistory.length-1]);}
+     return;
+    }
+    var packet = {};
+    var o = $('#inputForm').formToArray(true);
+    for (var k in o) {packet[o[k].name] = o[k].value;}
+    packet.tags = $.chat.tags;
+    if (($.chat.cmdHistory.length == 0) || ($.chat.cmdHistory[$.chat.cmdHistory.length-1] != $('#inputMessage').val()))
+    {
+     $.chat.cmdHistory[$.chat.cmdHistory.length] = $('#inputMessage').val();
+    }
+    $.chat.cmdHistoryPointer = $.chat.cmdHistory.length;
+    $.chat.sendMessage(packet);
+    var s = '';
+    if ($.chat.lastRecipients != null)
+    {
+     var e = $('#inputMessage').val().split(': ',2);
+     if ((e[0] != null) && (e[0] == $.chat.lastRecipients)) {s = e[0]+': ';}
+    }
+    $('#inputMessage').val(s);
+    $('#inputMessage').focus();
+   },5);
+   return false;
+  });
+ },
+ "initLoginForm": function()
+ {
+  $('#inputUsername').keyboard('esc',function (event) {$('#inputUsername').val('');})
+  $('#loginForm').submit(function()
+  { 
+   setTimeout(function()
+   {
+    $.chat.setUsername($('#inputUsername').val());
+   },5);
+   return false;
+  });
+ },
+ "addMsg": function(o)
+ {
+  if (o.color == null) {o.color = 'black';}
+  var dateStr = (o.ts != null)?('['+(new Date(o.ts*1000).toTimeString().substring(0,8))+'] '):'';
+  if (o.mtype == 'status')
+  {
+   if (($.chat.userlist[o.from] != null) && ($.chat.userlist[o.from][o.sid] != null))
+   {
+    $.chat.userlist[o.from][o.sid].statusmsg = o.text;
+    $.chat.updatedUserlist();
+   }
+   var s = '<p style="color: '+htmlspecialchars(o.color)+'">'+dateStr+'* <a href="#" style="color: '+htmlspecialchars(o.color)+'" onclick="$.chat.setRecipient('+htmlspecialchars($.toJSON(o.from))+'); return false">'+htmlspecialchars(o.from)+'</a> '+htmlspecialchars(o.text)+'</p>';
+  }
+  else if (o.mtype == 'astatus')
+  {
+   var s = '<p style="color: '+htmlspecialchars(o.color)+'">'+dateStr+'* <a href="#" style="color: '+htmlspecialchars(o.color)+'" onclick="$.chat.setRecipient('+htmlspecialchars($.toJSON(o.from))+'); return false">'+htmlspecialchars(o.from)+'</a> '+htmlspecialchars(o.text)+'</p>';
+  }
+  else if (o.mtype == 'system')
+  {
+   var s = '<p style="color: '+htmlspecialchars(o.color)+'">'+dateStr+' '+htmlspecialchars(o.text)+'</p>';
+  }
+  else
+  {
+   var style = 'color: '+htmlspecialchars(o.color)+';';
+   if ((o.to != null) && (o.to.indexOf($.chat.username) != -1)) {style += ' font-weight: bold;';}
+   var s = '<p style="'+style+'">'+dateStr+'<a href="#" style="color: '+htmlspecialchars(o.color)+'" onclick="$.chat.setRecipient('+htmlspecialchars($.toJSON(o.from))+'); return false">&lt;'+htmlspecialchars(o.from)+'&gt;</a>: '+htmlspecialchars(o.text)+'</p>';
+  }
+  $('#messages').append(s);
+  $('#messages').scrollTo('100%',{"axis": "y"});
+ },
+ "sendMessage": function(o)
+ {
+  o.cmd = "sendMessage";
+  $.chat.sendPacket(o);
+ },
+ "getHistory": function()
+ {
+  $.chat.sendPacket({
+   "cmd": "getHistory",
+   "tags": $.chat.tags,
+   "lastTS": $.chat.lastTS
+  });
+ },
+ "getUserlist": function()
+ {
+  if ($.chat.userlistUpdateTimeout != null)
+  {
+   clearTimeout($.chat.userlistUpdateTimeout);
+  }
+  $.chat.sendPacket({
+   "cmd": "getUserlist",
+   "tags": $.chat.tags
+  });
+  $.chat.userlistUpdateTimeout = setTimeout($.chat.getUserlist,25000);
+ },
+ "setUsername": function(username)
+ {
+  $.chat.sendPacket({
+   "cmd": "setUsername",
+   "username": username
+  });
+ },
+ "connect": function()
+ {
+  if ($.chat.ws != null)
+  {
+   if ($.chat.ws.readyState != 2) {return;}
+   else
+   {
+    $.chat.addMsg({"text": "* Пытаюсь переподключиться...", "color": "gray", "mtype": "system"});
+   }
+  }
+  $.chat.addMsg({"text": "* Идёт подключение...", "color": "gray", "mtype": "system"});
+  //$.chat.ws = new WebSocket($.chat.serverUrl.ws);
+  $.chat.ws = new WebSocketConnection({url: $.chat.serverUrl});
+  $.chat.ws.root = '/files/';
+  $.chat.ws.onopen = function()
+  {
+   $.chat.addMsg({"text": "* Подключение прошло успешно.", "color": "gray", "mtype": "system"});
+   if ($.chat.username != null) {$.chat.setUsername($.chat.username);}
+   $.chat.setTags($.chat.tagsDefault);
+   $.chat.getHistory();
+   $.chat.getUserlist();
+   setInterval(function()
+   {
+    $.chat.keepalive();
+   },20000);
+  };
+  $.chat.ws.onmessage = function(e)
+  {
+   if (e.data == null) {return;}
+   $.chat.recvBytes += e.data.length;
+   var o = $.parseJSON($.urldecode(e.data));
+   if (o.type == 'msg')
+   {
+    if (o.ts) {$.chat.lastTS = o.ts;}
+    $.chat.addMsg(o);
+   }
+   else if (o.type == 'tags')
+   {
+    $.chat.tags = o.tags;
+    $('.yourtags').text($.chat.tags.toString());
+    $.chat.getUserlist();
+   }
+   else if (o.type == 'userlist')
+   {
+    var newlist = {};
+    for (var k in o.userlist)
+    {
+     var u = o.userlist[k].username;
+     if (newlist[u] == null) {newlist[u] = {};}
+     newlist[u][o.userlist[k].id] = o.userlist[k];
+    };
+    $.chat.userlist = newlist;
+    $.chat.updatedUserlist();
+   }
+   else if (o.type == 'cstatus')
+   {
+    $.chat.username = o.username;
+    $.chat.setForms();
+   }
+   else if (o.type == 'joinsUser')
+   {
+    if (o.history == true) {return;}
+    if ($.chat.userlist[o.username] == null) {$.chat.userlist[o.username] = {};}
+    $.chat.userlist[o.username][o.sid] = {"username": o.username, "sid": o.sid, "tags": o.tags, "statusmsg": o.statusmsg};
+    $.chat.updatedUserlist();
+   }
+   else if (o.type == 'partsUser')
+   {
+    if (o.history == true) {return;}
+    if ($.chat.userlist[o.username] == null) {return;}
+    if ($.chat.userlist[o.username][o.sid] == null) {return;}
+    delete $.chat.userlist[o.username][o.sid];
+    var found = false;
+    for (var k in $.chat.userlist[o.username])
+    {
+     found = true;
+     break;
+    }
+    if (!found) {delete $.chat.userlist[o.username];}
+    $.chat.updatedUserlist();
+   }
+   else if (o.type == 'changedUsername')
+   {
+    if (o.history == true) {return;}
+    if ($.chat.userlist[o.old] == null) {return;}
+    if ($.chat.userlist[o.old][o.sid] == null) {return;}
+    var n = o['new'];
+    if ($.chat.userlist[n] == null) {$.chat.userlist[n] = {};}
+    $.chat.userlist[n][o.sid] = $.chat.userlist[o.old][o.sid];
+    $.chat.userlist[n][o.sid].username = n;
+    delete $.chat.userlist[o.old][o.sid];
+    var found = false;
+    for (var k in $.chat.userlist[o.old])
+    {
+     found = true;
+     break;
+    }
+    if (!found) {delete $.chat.userlist[o.old];}
+    $.chat.updatedUserlist();
+   }
+  };
+  $.chat.ws.onclose = function()
+  {
+   $.chat.addMsg({"text": "* Упс! Произошло отключение от сервера!", "color": "gray", "mtype": "system"});
+  };
+ }
+};
+$.urlencode = function(s)
+{
+ if (typeof encodeURIComponent != 'undefined') {return encodeURIComponent(s).replace(new RegExp('\\+','g'), '%20');}
+ return escape(s).replace(new RegExp('\\+','g'), '%20');
+};
+$.urldecode = function(s)
+{
+ return unescape(s).replace(new RegExp('\\+','g'),' ');
+};
+if (typeof WebSocket != 'undefined') {WebSocket.__swfLocation = "/files/websocket/WebSocketMain.swf";}
+$(document).ready(function()
+{
+ var tabs,
+ tabsSelector = '#nav ul:first li:all a:all',
+ defaultClass = 'ui-state-default',
+ activeClass = 'ui-state-active';
+ $.address.init(function(event) {
+                // Init flicker fix
+                $(tabsSelector).removeClass(activeClass)
+                   .addClass(defaultClass);
+                   var c = $('a[rel=address:' + $.address.path + ']');
+                if (typeof (c[0]) != 'undefined') {
+                 $(c[0]).removeClass(defaultClass).addClass(activeClass);
+                }
+               
+            }).change(function(event) {
+                var selection = $('a[rel=address:' + event.path + ']');
+                $(tabsSelector).removeClass(activeClass).addClass(defaultClass);
+                selection.removeClass(defaultClass).addClass(activeClass);
+                $.address.title($.address.title().split(' | ')[0] + ' | ' + selection.text());
+                if (event.path === '/') {event.path = '/index';}
+                $('#content').load('/pages'+event.path+'.html',null,function (responseText, textStatus, XMLHttpRequest)
+                {
+                 if (textStatus == 'error')
+                 {
+                  $('#content').html('Упс! Произошла ошибка. Проверьте Ваше подключение.');
+                 }
+                });
+            });
+});
+function htmlspecialchars(s)
+{
+ if (s == null) {return 'null';}
+ var r = s;
+ r = r.replace(new RegExp('&','g'),'&amp;');
+ r = r.replace(new RegExp('"','g'),'&quot;');
+ r = r.replace(new RegExp('\'','g'),'&#039;');
+ r = r.replace(new RegExp('<','g'),'&lt;');
+ r = r.replace(new RegExp('>','g'),'&gt;');
+ return r;
+}
+function oldhtmlspecialchars(string)
+{
+ if (string == null) {return "";}
+ return $('<span>').text(string).html();
+}
+function fsize(x)
+{
+ if (x >= 1024*1024*1024) {return (Math.floor(x/1024/1024/1024*100)/100)+' Gb';}
+ if (x >= 1024*1024) {return (Math.floor(x/1024/1024*100)/100)+' Mb';}
+ if (x >= 1024) {return (Math.floor(x/1024*100)/100)+' Kb';}
+ return (x)+' B';
+}
+function Dump(d, l, t) {
+ if (typeof(t) == "undefined") t = "\n";
+ var space = (t == "\n")?' ':'&nbsp;';
+
+    if (l == null) l = 1;
+    var s = '';
+
+    if (typeof(d) == "object") {
+        s += typeof(d) + space+"{"+t;
+        for (var k in d) {
+            if (typeof(d[k]) != "function"){
+             for (var i=0; i<l; i++) s += space+space;
+             s += k+":"+space + Dump(d[k],l+1, t);
+            }
+        }
+        for (var i=0; i<l-1; i++) s += space+space;
+        s += "}"+t;
+    } else if (typeof(d) != "function"){
+        s += "" + d + t;
+    }
+    return s;
+}
---
+++ html/files/websocket.js
@@ -0,0 +1,326 @@
+﻿/**
+   params = {
+              url   : {
+                        ws      : '',
+                        comet   : '',
+                        polling  : ''
+                      }
+            }
+**/
+
+
+WebSocketConnection = function(params){
+
+    var self = this;
+
+    /**
+     * Путь к js и swf файлам
+     */
+    this.root = '/files/';
+    
+
+
+    /**
+     * WebSocket соединение
+     */
+    var WS = null;
+
+
+
+    /**
+     * Статус WebSocket соединения
+     */
+    this.readyState     = 3;
+    this.bufferedAmount = 0;
+
+
+
+    /**
+     * Закрытие соединения с сервером
+     */
+    this.close = function(){if(WS)WS.close();};
+
+
+
+    /**
+     * Отправка данных на сервер
+     */
+    this.send = function(data){if(WS)WS.send(data);};
+    
+
+
+    /**** События  ****/
+
+    // получен новый пакет данных
+    var onmessageEvent = function(e){if(self.onmessage)self.onmessage(e);};
+
+    // соединение с сервером установлено
+    var onopenEvent = function(){if(self.onopen)self.onopen();};
+
+    // соединение с сервером закрыто
+    var oncloseEvent = function(){if(self.onclose)self.onclose();};
+
+
+
+
+
+
+    /**
+     * Загрузка файла
+     */
+    var loadFile = function(file){
+           var script = document.createElement('script');
+               script.type = 'text/javascript';
+               script.src = self.root + file;
+           var c = document.head || document.body;
+           c.appendChild(script);
+    };
+
+
+
+
+
+
+    /**
+     * Загрузка драйвера
+     */
+    var loadEmulator = function(type, onLoaded){
+
+          if(type == 'flash'){   // загружаем флешовый эмулятор
+
+             WebSocket__swfLocation = self.root + 'flash.swf';  // путь к swf файлу
+
+             if(!("FABridge" in window))loadFile('fabridge.js');   // если не загружен FABridge, загружаем
+
+             if(!("swfobject" in window))loadFile('swfobject.js'); // если не загружен swfobject, загружаем
+
+             var interval1 = setInterval(function(){
+                 if(("FABridge" in window) && ("swfobject" in window)){
+                    clearInterval(interval1);
+                    loadFile('websocket_flash.js');
+                 }
+             },10);
+
+          }else if(type == 'comet'){    // загружаем comet эмулятор
+
+              loadFile('websocket_comet.js');
+
+          }else if(type == 'polling'){   // загружаем long-poling эмулятор
+
+              loadFile('websocket_polling.js');
+
+          }else{
+            alert('Error loadEmulator');
+            return;
+          }
+
+           var interval2 = setInterval(function(){
+                  if("WebSocket" in window){
+                       clearInterval(interval2);
+                       onLoaded();
+                  }
+           }, 5);
+    };
+
+
+
+
+
+    /**
+     * Проверка на доступность драйверов
+     */
+    var loadDriver = function(driver, onError){
+
+           if(!driver)onError();
+           var loaded = false;
+           switch(driver){
+
+             case 'ws'     : loaded = WSDriver();
+             break;
+
+             case 'comet'  : loaded = CometDriver();
+             break;
+
+             case 'polling' : loaded = PollingDriver();
+             break;
+           }
+
+           if(!loaded){
+             onError();
+           }
+    }
+
+
+
+
+    /**
+     * Проверка на доступность нативного WebSocket или Flash плеера
+     */
+    var WSDriver = function(){
+
+        if("WebSocket" in window){
+          createSocket(params.url.ws);
+          return true;
+         }
+
+          var x, flashinstalled = 0;
+               var MSDetect = false;
+
+            if (navigator.plugins && navigator.plugins.length){
+                x = navigator.plugins["Shockwave Flash"];
+                if(x){
+                    flashinstalled = 2;
+                }else{
+                    flashinstalled = 1;
+                }
+
+                if (navigator.plugins["Shockwave Flash 9.0"] || navigator.plugins["Shockwave Flash 10.0"]){
+                    flashinstalled = 2;
+                }
+
+            }else if (navigator.mimeTypes && navigator.mimeTypes.length){
+
+                x = navigator.mimeTypes['application/x-shockwave-flash'];
+                if (x && x.enabledPlugin)
+                    flashinstalled = 2;
+                else
+                    flashinstalled = 1;
+            }else
+                MSDetect = true;
+
+
+            if(flashinstalled != 2 && MSDetect == true){
+               var iframe = document.createElement('iframe');
+                 with (iframe.style) {
+                        left       = top   = "-100px";
+                        height     = width = "1px";
+                        position   = 'absolute';
+                        display    = 'none';
+                      }
+                 document.body.appendChild(iframe);
+               var win;
+                 if(typeof(iframe.contentWindow) != 'undefined'){
+                    win = iframe.contentWindow.window;
+                 }else if(iframe.window){
+                   win = iframe.window;
+                 }
+
+                 if(win){
+                   win.flashinstalled = 0;
+                   win.document.write("<html><body>");
+                   win.document.write('<script LANGUAGE="VBScript">');
+                   win.document.write('on error resume next');
+                   win.document.write('For i = 1 to 11');
+                   win.document.write('If Not(IsObject(CreateObject("ShockwaveFlash.ShockwaveFlash." & i))) Then');
+                   win.document.write('Else');
+                   win.document.write('flashinstalled = 2');
+                   win.document.write('End If');
+                   win.document.write('Next');
+                   win.document.write('If flashinstalled = 0 Then');
+                   win.document.write('flashinstalled = 1');
+                   win.document.write('End If');
+                   win.document.write('End If');
+                   win.document.write('</script>');
+                   win.document.write("</body></html>");
+                   flashinstalled = win.flashinstalled;
+                 }
+            }
+
+              if(flashinstalled == 2){
+               loadEmulator('flash', function(){
+                  createSocket(params.url.ws);
+               });
+               return true;
+              }
+         return false;
+    }
+
+
+
+
+
+    /**
+     * Проверка на доступность драйвера comet
+     */
+    var CometDriver = function(){
+
+       if("WebSocket" in window && ("WebSocketServicePrivider" in window) && WebSocketServicePrivider == 'comet'){
+        	  createSocket(params.url.comet);
+         }else{
+           loadEmulator('comet', function(){
+              createSocket(params.url.comet);
+           });
+         }
+
+         return true;
+    }
+
+
+
+
+    /**
+     * Проверка на доступность драйвера long-polling
+     */
+     var PollingDriver = function(){
+
+         if("WebSocket" in window && ("WebSocketServicePrivider" in window) && WebSocketServicePrivider == 'polling'){
+        	  createSocket(params.url.polling);
+         }else{
+           loadEmulator('polling', function(){
+              createSocket(params.url.polling);
+           });
+         }
+         return true;
+    }
+
+
+
+
+
+    /**
+     * Иннициализируем вебсокет
+     */
+    var createSocket = function(url){
+              WS           = new WebSocket(url);
+              WS.onopen    = onopenEvent;
+              WS.onmessage = onmessageEvent;
+              WS.onclose   = oncloseEvent;
+    }
+
+
+
+
+
+
+     var priority = [];
+
+        var i = 0;
+         for(var type in params.url){
+           priority[i] = type;
+           i++;
+         };
+
+    if(typeof(priority[0]) != undefined){
+
+       loadDriver(priority[0],function(){
+
+         if(typeof(priority[1]) != undefined){
+
+          loadDriver(priority[1],function(){
+
+            if(typeof(priority[2]) != undefined){
+
+              loadDriver(priority[2],function(){
+
+               });
+             }
+           });
+         }
+      });
+    }
+
+
+
+};
+
+
